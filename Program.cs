ec082e862cac49f7fdf0ac33d258c85640158f78
Kacper Szatkowski <kacper.szatkowski@wp.pl>
2023-12-26 13:11:52 +0100

    user parameters validation

--- Program.cs
+++ Program.cs
@@ -7,10 +7,29 @@ namespace Kalkulator_BMI
     {
         public static void Main()
         {
+            height:
             Console.WriteLine("Podaj swój wzrost w centymetrach:");
             var height = UserInput.AddParameter();
+            if (height <= 220 && height >= 100)
+            {
+                
+            }
+            else
+            {
+                goto height;
+            }
+
+            weight:
             Console.WriteLine("Podaj swoją wagę w kilogramach:");
             var weight = UserInput.AddParameter();
+            if (weight <= 250 && weight >= 20)
+            {
+
+            }
+            else
+            {
+                goto weight;
+            }
 
             var result = weight / Math.Pow(height / 100f, 2);
             result = Math.Round(result, 2);
